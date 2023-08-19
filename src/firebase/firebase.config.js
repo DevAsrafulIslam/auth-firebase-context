4997e124c881601168f49ab31329954e007fe19c
Asraful Islam <asrafulislam@Asrafuls-MacBook-Pro.local>
2023-08-19 14:19:28 +0600

    implement login and registation

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB9C1jZOKMkqjJqqatiHaEhnWmq8DO21Zc",
+  authDomain: "auth-firebase-context-50b23.firebaseapp.com",
+  projectId: "auth-firebase-context-50b23",
+  storageBucket: "auth-firebase-context-50b23.appspot.com",
+  messagingSenderId: "930109262232",
+  appId: "1:930109262232:web:820a60c04cdbd7c67d83ed",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
